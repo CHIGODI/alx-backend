e8c3df73f40fda9b99e16043ed55a7512a0df235
CHIGODI <chiegody254@gmail.com>
2024-07-30 13:51:19 +0300

    Add strict_slashes false

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Renders simple html page"""
     return render_template('0-index.html')
