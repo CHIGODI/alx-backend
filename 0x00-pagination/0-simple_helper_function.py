9ddcbb86788dadf99b2215283d09a8f2fe0310a8
CHIGODI <chiegody254@gmail.com>
2024-08-06 10:01:16 +0300

    Fix pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Write a function named index_range that takes two integer arguments page and page_size.
+Write a function named index_range that takes two integer
+arguments page and page_size.
 
 The function should return a tuple of size two containing
 a start index and an end index corresponding to the range of
