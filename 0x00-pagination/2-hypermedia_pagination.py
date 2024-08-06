0dd19204501ce2cb28f0e894174a86f429391cb1
CHIGODI <chiegody254@gmail.com>
2024-08-06 11:20:55 +0300

    Add new logic

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -47,6 +47,25 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Retrieves information about a page.
         """
+        # data = self.get_page(page, page_size)
+        # next_page = None
+        # prev_page = None
+        # total_pages =math.ceil(len(self.dataset())/page_size)
+        # if page + 1 < total_pages:
+        #     next_page = page + 1
+        # if (page - 1) > 1:
+        #     prev_page = page - 1
+
+        # hyper_dict ={
+        #     "page_size": len(data),
+        #     "page": page,
+        #     "data": data,
+        #     "next_page": next_page,
+        #     "prev_page": prev_page,
+        #     "total_pages": total_pages
+        # }
+
+        # return hyper_dict
         data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
